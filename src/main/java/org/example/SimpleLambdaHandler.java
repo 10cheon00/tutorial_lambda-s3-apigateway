49cf0ac81ce0bd292a9237c7e11ecc2dbdeaa703
10cheon00 <10cheon00@gmail.com>
2025-01-02 23:15:40 +0900

    프로젝트 파일 정리

--- src/main/java/org/example/SimpleLambdaHandler.java
+++ src/main/java/org/example/SimpleLambdaHandler.java
@@ -1,13 +1,11 @@
-package main.java.helloworld;
+package org.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SimpleLambdaHandler implements RequestHandler<String, String> {
-
     @Override
-    public String handleRequest(String input, Context context) {
-        context.getLogger().log("Input: " + input);
-        return "Hello, " + input + "! Welcome to AWS Lambda.";
+    public String handleRequest(String s, Context context) {
+        return "Test";
     }
 }
