a0a6ac3153962e87cdfaac10088c9c5351436b4b
10cheon00 <10cheon00@gmail.com>
2025-01-02 20:04:23 +0900

    코드 변경

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,27 +1,24 @@
 package helloworld;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import org.json.simple.JSONObject;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.io.*;
+// Handler value: example.HandlerInteger
+public class HandlerIntegerJava17 implements RequestHandler<IntegerRecord, Integer>{
 
-public class Handler implements RequestStreamHandler{
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream,
-                              Context context) throws IOException {
-
-        JSONObject obj = new JSONObject();
-        JSONObject obj2 = new JSONObject();
-        obj2.put("Content-Type", "application/json");
-        obj.put("statusCode", 200);
-        obj.put("headers", obj2);
-        obj.put("body", "hello world");
-
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-        writer.write(obj.toString());
-        writer.close();
-
+    /*
+     * Takes in an InputRecord, which contains two integers and a String.
+     * Logs the String, then returns the sum of the two Integers.
+     */
+    public Integer handleRequest(IntegerRecord event, Context context)
+    {
+        LambdaLogger logger = context.getLogger();
+        logger.log("String found: " + event.message());
+        return event.x() + event.y();
     }
+}
 
+record IntegerRecord(int x, int y, String message) {
 }
