3438bf6f5d47de6417fce3bb480a2aed18ab4b95
10cheon00 <10cheon00@gmail.com>
2025-01-02 21:27:53 +0900

    자바핸들러 코드 테스트

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,24 +1,13 @@
-package helloworld;
+package main.java.helloworld;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-// Handler value: example.HandlerInteger
-public class HandlerIntegerJava17 implements RequestHandler<IntegerRecord, Integer>{
+public class SimpleLambdaHandler implements RequestHandler<String, String> {
 
     @Override
-    /*
-     * Takes in an InputRecord, which contains two integers and a String.
-     * Logs the String, then returns the sum of the two Integers.
-     */
-    public Integer handleRequest(IntegerRecord event, Context context)
-    {
-        LambdaLogger logger = context.getLogger();
-        logger.log("String found: " + event.message());
-        return event.x() + event.y();
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello, " + input + "! Welcome to AWS Lambda.";
     }
-}
-
-record IntegerRecord(int x, int y, String message) {
 }
