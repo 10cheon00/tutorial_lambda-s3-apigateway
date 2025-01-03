1358eacf6920aa113968d95b74ce368582d896a6
10cheon00 <10cheon00@gmail.com>
2025-01-03 21:01:16 +0900

    텍스트 번역 성공

--- handler.py
+++ handler.py
@@ -6,6 +6,8 @@ translate = boto3.client('translate')
 def lambda_handler(event, context):
     print(f'{event.get('body')}')
     request_body = json.loads(event.get('body'))
+    statusCode = None
+    response_body = None
     try:
         target_lang = request_body['target_language']
         text = request_body['text']
@@ -14,6 +16,7 @@ def lambda_handler(event, context):
             SourceLanguageCode="auto",
             TargetLanguageCode=target_lang
         )
+        print(f"result: {result}")
         statusCode = 200
         response_body = {
             "result": result.get("TranslatedText")
@@ -23,9 +26,12 @@ def lambda_handler(event, context):
     except Exception as e:
         statusCode = 500
         response_body = {
-            "message": e.__str__
+            "message": e.__str__()
         }
+        print(e.__str__())
     finally:
+        print(statusCode)
+        print(response_body)
         return {
             'statusCode': statusCode,
             'headers': 'application/json',
