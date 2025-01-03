e36800d0b4dbc27a8ebd61581bdc768db49aa0d0
10cheon00 <10cheon00@gmail.com>
2025-01-03 19:04:42 +0900

    String 반환 테스트

--- src/main/java/org/example/ResponseBody.java
+++
@@ -1,4 +0,0 @@
-package org.example;
-
-public record ResponseBody(String body, Integer statusCode) {
-}
--- src/main/java/org/example/SimpleLambdaHandler.java
+++ src/main/java/org/example/SimpleLambdaHandler.java
@@ -4,11 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class SimpleLambdaHandler implements RequestHandler<RequestBody, ResponseBody> {
+public class SimpleLambdaHandler implements RequestHandler<RequestBody, String> {
     @Override
-    public ResponseBody handleRequest(RequestBody requestBody, Context context) {
+    public String handleRequest(RequestBody requestBody, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log(requestBody.text());
-        return new ResponseBody(requestBody.text(), 200);
+        return requestBody.text();
     }
 }
