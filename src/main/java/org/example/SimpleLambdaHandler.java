9bcac050ddea2efd528cde8561e951b96ba56d98
10cheon00 <10cheon00@gmail.com>
2025-01-03 18:17:47 +0900

    로그 추가

--- src/main/java/org/example/SimpleLambdaHandler.java
+++ src/main/java/org/example/SimpleLambdaHandler.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SimpleLambdaHandler implements RequestHandler<RequestBody, String> {
     @Override
     public String handleRequest(RequestBody requestBody, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log(requestBody.text());
         return requestBody.text();
     }
 }
