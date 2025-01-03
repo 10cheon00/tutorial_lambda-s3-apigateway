d6ad9fd7c6a0bdb2e544b7f0e2686d01f973888c
10cheon00 <10cheon00@gmail.com>
2025-01-03 18:37:52 +0900

    응답 형태 변경 테스트

--- src/main/java/org/example/SimpleLambdaHandler.java
+++ src/main/java/org/example/SimpleLambdaHandler.java
@@ -4,11 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class SimpleLambdaHandler implements RequestHandler<RequestBody, String> {
+public class SimpleLambdaHandler implements RequestHandler<RequestBody, ResponseBody> {
     @Override
-    public String handleRequest(RequestBody requestBody, Context context) {
+    public ResponseBody handleRequest(RequestBody requestBody, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log(requestBody.text());
-        return requestBody.text();
+        return new ResponseBody(requestBody.text(), 200);
     }
 }
