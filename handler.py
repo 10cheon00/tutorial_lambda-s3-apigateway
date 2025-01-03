c3fb019fff60e38ef2598a3ea7f849b36ca45554
10cheon00 <10cheon00@gmail.com>
2025-01-03 20:41:49 +0900

    translate api 사용

--- handler.py
+++ handler.py
@@ -1,14 +1,33 @@
 import json
+import boto3
+
+translate = boto3.client('translate')
 
 def lambda_handler(event, context):
     print(f'{event.get('body')}')
-    req = json.loads(event.get('body'))
-    
-    print(req['text'])
-    body = req['text']
-    
-    return {
-        'statusCode': 200,
-        'headers': 'application/json',
-        'body': json.dumps(body)
-    }
+    request_body = json.loads(event.get('body'))
+    try:
+        target_lang = request_body['target_language']
+        text = request_body['text']
+        result = translate.translate_text(
+            Text=text,
+            SourceLanguageCode="auto",
+            TargetLanguageCode=target_lang
+        )
+        statusCode = 200
+        response_body = {
+            "result": result.get("TranslatedText")
+        }
+        
+
+    except Exception as e:
+        statusCode = 500
+        response_body = {
+            "message": e.__str__
+        }
+    finally:
+        return {
+            'statusCode': statusCode,
+            'headers': 'application/json',
+            'body': json.dumps(response_body)
+        }
